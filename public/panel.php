662db6e9743b7d01cf33de61dc2c3a5c969df9f2
Danny Monroy <danny.p.monroy@gmail.com>
2017-04-11 11:05:17 +0100

    Added a log out button in the nav bar

--- public/panel.php
+++ public/panel.php
@@ -43,6 +43,9 @@
           <li class="nav-item">
             <a class="nav-link" href="#">Help</a>
           </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.html">Log out</a>
+          </li>
         </ul>
       </div>
     </nav>

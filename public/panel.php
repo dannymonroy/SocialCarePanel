79354e6345add6b958d82f10b198b69987681f66
Danny Monroy <danny.p.monroy@gmail.com>
2017-04-13 11:37:13 +0100

    Added a TODO section in the INDEX.HTML. Refactor create_cient, create_visits...etc. Chaning create to new.

--- public/panel.php
+++ public/panel.php
@@ -10,21 +10,21 @@
              <h1>Dashboard</h1>
              <section class="row text-center placeholders">
                <div class="col-6 col-sm-3 placeholder">
-                 <a href="create_client.php">
+                 <a href="new_client.php">
                  <img src="data:image/gif;base64,R0lGODlhAQABAIABAAJ12AAAACwAAAAAAQABAAACAkQBADs=" width="200" height="200" class="img-fluid rounded-circle" alt="Generic placeholder thumbnail">
                  </a>
                  <h4>Clients</h4>
                  <div class="text-muted">Add or view clients</div>
                </div>
                <div class="col-6 col-sm-3 placeholder">
-                 <a href="create_carer.php">
+                 <a href="new_carer.php">
                  <img src="data:image/gif;base64,R0lGODlhAQABAIABAADcgwAAACwAAAAAAQABAAACAkQBADs=" width="200" height="200" class="img-fluid rounded-circle" alt="Generic placeholder thumbnail">
                  </a>
                  <h4>Carers</h4>
                  <span class="text-muted">Add or view carers</span>
                </div>
                <div class="col-6 col-sm-3 placeholder">
-                 <a href="create_visit.php">
+                 <a href="new_visit.php">
                  <img src="data:image/gif;base64,R0lGODlhAQABAIABAAJ12AAAACwAAAAAAQABAAACAkQBADs=" width="200" height="200" class="img-fluid rounded-circle" alt="Generic placeholder thumbnail">
                  </a>
                  <h4>Visits</h4>

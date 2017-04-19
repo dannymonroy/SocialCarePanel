8170544c87770057452c9e91b68af91b4ed8d0b3
Danny Monroy <danny.p.monroy@gmail.com>
2017-04-19 20:46:22 +0100

    Added the PHP necessary to authenticate a login to the Android app

--- login.php
+++ login.php
@@ -1,23 +1,32 @@
 <?php
-require_once("includes/db_connection.php");
-
+	require_once("includes/db_connection.php");
 	if($_SERVER['REQUEST_METHOD']=='POST'){
+		$carer_id = $_POST["carer_id"];
+		$password = $_POST["password"];
 
-		$username = $_POST['username'];
-		$password = $_POST['password'];
+
+		//$carer_id = "brwayn";
+		//$password = "batman";
+
+	  $statement = mysqli_prepare($connection, "SELECT carer_id FROM Carers WHERE carer_id = ? AND password = ?");
+
+		mysqli_stmt_bind_param($statement, "ss", $carer_id, $password);
+    mysqli_stmt_execute($statement);
+    mysqli_stmt_store_result($statement);
+    @mysqli_stmt_bind_result($statement, $carer_id, $password);
+	 // $result = mysqli_query($connection, $query);
 
 
-		$query = "SELECT carer_id FROM Carers WHERE carer_id = '$username' AND password ='$password'";
 
-		$result = mysqli_query($connection,$query);
+		$response = array();
+		$response["success"] = false;
 
-		$check = mysqli_fetch_array($result);
-
-		if(isset($check)){
-			echo 'success';
-		}else{
-			echo 'failure';
+		while(mysqli_stmt_fetch($statement)){
+						$response["success"] = true;
+						$response["carer_id"] = $carer_id;
 		}
-	}
+		echo json_encode($response);
+
+}
 
 ?>

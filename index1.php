f5c08da9f7c212d436a61f2068b1497f5a9e613d
Danny Monroy <danny.p.monroy@gmail.com>
2017-04-20 19:47:11 +0100

    Changed the API query so it returns the JSON file in order of start_time

--- index1.php
+++ index1.php
@@ -7,7 +7,8 @@
   $query .= " Clients.title, Clients.name, Clients.middle_name, Clients.surname, Clients.area, Visits.start_time, Visits.end_time, Clients.address, Clients.postcode, Clients.general_information, Clients.keycode, Clients.level_vulnerability  ";
   $query .= " FROM Clients ";
   $query .= " JOIN Visits ON Visits.client_id = Clients.client_id ";
-  $query .= " WHERE Visits.carer_id = '".$carer."';";
+  $query .= " WHERE Visits.carer_id = '".$carer."' ";
+  $query .= " ORDER BY Visits.start_time;";
 
   $result = mysqli_query($connection, $query);
 

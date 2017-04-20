3c417106c589204f14d883f6b8846723baae0c0f
Danny Monroy <danny.p.monroy@gmail.com>
2017-04-20 10:03:00 +0100

    Changed the query so all information about a client is send to the Android application

--- index1.php
+++ index1.php
@@ -4,7 +4,7 @@
   $carer = $_GET['id'];
 
   $query = "SELECT ";
-  $query .= " Clients.title, Clients.name, Clients.surname, Clients.area, Visits.start_time ";
+  $query .= " Clients.title, Clients.name, Clients.middle_name, Clients.surname, Clients.area, Visits.start_time, Visits.end_time, Clients.address, Clients.postcode, Clients.general_information, Clients.keycode, Clients.level_vulnerability  ";
   $query .= " FROM Clients ";
   $query .= " JOIN Visits ON Visits.client_id = Clients.client_id ";
   $query .= " WHERE Visits.carer_id = '".$carer."';";

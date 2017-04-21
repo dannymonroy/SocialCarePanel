ac9e9c06c3e19d69ab138c20b23fea4e9e3e1cdc
Danny Monroy <danny.p.monroy@gmail.com>
2017-04-21 20:38:32 +0100

    Added a comment for login.php and a reference to tonikami

--- login.php
+++ login.php
@@ -1,32 +1,29 @@
 <?php
 	require_once("includes/db_connection.php");
+	/*
+	Script connects to the Android Social Care Application. It retrieves a carer id and a password to do the log in.
+	it then checks the database and returns a json object with the id (for further use by index1.php) and a boolean if the login was
+	succesful or not.	Script based on https://github.com/tonikami PHP work
+	*/
 	if($_SERVER['REQUEST_METHOD']=='POST'){
 		$carer_id = $_POST["carer_id"];
 		$password = $_POST["password"];
 
+		$json = array();
+		$json["success"] = false;
 
-		//$carer_id = "brwayn";
-		//$password = "batman";
+	  $sql = mysqli_prepare($connection, "SELECT carer_id FROM Carers WHERE carer_id = ? AND password = ?");
 
-	  $statement = mysqli_prepare($connection, "SELECT carer_id FROM Carers WHERE carer_id = ? AND password = ?");
+		mysqli_stmt_bind_param($sql, "ss", $carer_id, $password);
+    mysqli_stmt_execute($sql);
+    mysqli_stmt_store_result($sql);
+    @mysqli_stmt_bind_result($sql, $carer_id, $password);
 
-		mysqli_stmt_bind_param($statement, "ss", $carer_id, $password);
-    mysqli_stmt_execute($statement);
-    mysqli_stmt_store_result($statement);
-    @mysqli_stmt_bind_result($statement, $carer_id, $password);
-	 // $result = mysqli_query($connection, $query);
-
-
-
-		$response = array();
-		$response["success"] = false;
-
-		while(mysqli_stmt_fetch($statement)){
-						$response["success"] = true;
-						$response["carer_id"] = $carer_id;
+		while(mysqli_stmt_fetch($sql)){
+						$json["success"] = true;
+						$json["carer_id"] = $carer_id;
 		}
-		echo json_encode($response);
-
+		echo json_encode($json);
 }
 
 ?>

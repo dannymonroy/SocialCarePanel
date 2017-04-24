e235876cd858a02161424b3952ed9251f31fa9bb
Danny Monroy <danny.p.monroy@gmail.com>
2017-04-24 15:35:24 +0100

    Added visual improvements in reports.php

--- public/reports.php
+++ public/reports.php
@@ -3,13 +3,15 @@
 <?php include("../includes/layouts/header.php");?>
 <?php @changeToHTTP(); ?>
 <div class="container justify-center">
-  <h2>View Reports</h2>
-  <p class="text-justify"> <strong>Number of clients: </strong></p><h3>
+  <h2 class="margin-top">View Reports</h2>
+
+  <p class="text-justify"> <strong>Number of clients: </strong></p><h3 class="margin-top">
     <?php
     echo displayNumClients();
     ?></h3>
-  <p class="text-justify"> <strong>Visits added: </strong>The below graph shows the amount of visits given to your clients on a daily basis.</p>
-  <div id="visualization"></div>
+  <p class="text-justify"> <strong>Visits added: </strong> </p>
+  <p>The below graph shows the amount of visits given to your clients on a daily basis.</p>
+  <div id="visualization" class="margin-top"></div>
 
 <script type="text/javascript"> displayClientsGraph(); </script>
 </div>

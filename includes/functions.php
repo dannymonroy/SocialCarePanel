eedc53f7f747c9a1e3e33f89cd97d719ddeb39e0
Danny Monroy <danny.p.monroy@gmail.com>
2017-04-28 17:42:22 +0100

    Deleted a function not needed anymore

--- includes/functions.php
+++ includes/functions.php
@@ -14,15 +14,6 @@
     return $escaped_string;
   }
 
-  //This function will have to change but for now is for testing purposes
-  function displayName($username){
-    if(isset($username)){
-      return $username;
-    } else {
-      return "Stranger";
-    }
-  }
-
   function confirm_query($result_set){
     if (!$result_set){
       die("Database query failed");
